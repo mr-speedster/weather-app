bdaca458ec9fb668af7a57232849a8b727e7b11c
Ajith <ajithd78564@gmail.com>
2021-10-18 18:55:35 +0530

    root editon

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/',[WeatherController::class,'weather']);
-Route::post('/location',[WeatherController::class,'location']);
+Route::post('/',[WeatherController::class,'location']);
