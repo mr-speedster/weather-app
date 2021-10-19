b124932942ed1d9f7de658d61bd67909b0553867
Ajith <ajithd78564@gmail.com>
2021-10-19 13:44:27 +0530

    view updates

--- resources/views/weatherView.blade.php
+++ resources/views/weatherView.blade.php
@@ -7,12 +7,18 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="public/css/style.css">
     <title> Weather App</title>
   </head>
   <body>
-    <h1>{{$temp}}</h1>
+    <rain>
+      <div class="rain">
 
+      </div>
+    </rain>
+    <sun>
+
+    </sun>
     <!-- Optional JavaScript; choose one of the two! -->
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
